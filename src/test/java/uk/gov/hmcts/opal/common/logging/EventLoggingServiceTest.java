528542f72fb236e203e4b09bf2decacf4e0bf808
Russell Dodd <russell.dodd@hmcts.net>
2026-05-21 12:45:12 +0100

    Update Azure application insights dependencies in accordance with MS guidelines.

--- src/test/java/uk/gov/hmcts/opal/common/logging/EventLoggingServiceTest.java
+++ src/test/java/uk/gov/hmcts/opal/common/logging/EventLoggingServiceTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -21,11 +20,6 @@ import java.util.stream.Collectors;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Appender;
-import com.microsoft.applicationinsights.extensibility.context.OperationContext;
-import com.microsoft.applicationinsights.telemetry.RequestTelemetry;
-import com.microsoft.applicationinsights.telemetry.TelemetryContext;
-import com.microsoft.applicationinsights.web.internal.RequestTelemetryContext;
-import com.microsoft.applicationinsights.web.internal.ThreadContext;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Path;
@@ -33,6 +27,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -125,19 +121,21 @@ class EventLoggingServiceTest {
         when(config.getDomain()).thenReturn("TestDomain");
         when(clock.now()).thenReturn(now);
         String opId = UUID.randomUUID().toString().replace("-", "");
-        ThreadContext.setRequestTelemetryContext(mockRequestTelemetryContext(opId));
+        try (MockedStatic<LogUtil> logUtilMock = Mockito.mockStatic(LogUtil.class)) {
+            logUtilMock.when(LogUtil::getOrCreateOpalOperationId).thenReturn(opId);
 
-        // Act
-        service.logEvent("TestEvent", "Success", null, "TestType",null, null);
+            // Act
+            service.logEvent("TestEvent", "Success", null, "TestType", null, null);
 
-        // Assert
-        ArgumentCaptor<ILoggingEvent> captor = ArgumentCaptor.forClass(ILoggingEvent.class);
-        verify(mockAppender, atLeastOnce()).doAppend(captor.capture());
-        List<ILoggingEvent> events = captor.getAllValues();
-        String nowText = now.toString();
-        String compareTo = "TestEvent,Success," + nowText + ",TestDomain,," + opId + ",TestType," + nowText + ",";
-        events.stream().findFirst().ifPresentOrElse(
-            e -> assertEquals(compareTo, e.getFormattedMessage()), () -> fail("No Log event captured."));
+            // Assert
+            ArgumentCaptor<ILoggingEvent> captor = ArgumentCaptor.forClass(ILoggingEvent.class);
+            verify(mockAppender, atLeastOnce()).doAppend(captor.capture());
+            List<ILoggingEvent> events = captor.getAllValues();
+            String nowText = now.toString();
+            String compareTo = "TestEvent,Success," + nowText + ",TestDomain,," + opId + ",TestType," + nowText + ",";
+            events.stream().findFirst().ifPresentOrElse(
+                e -> assertEquals(compareTo, e.getFormattedMessage()), () -> fail("No Log event captured."));
+        }
     }
 
     @Test
@@ -147,7 +145,6 @@ class EventLoggingServiceTest {
         when(config.getDomain()).thenReturn("TestDomain");
         when(clock.now()).thenReturn(now);
         String opId = UUID.randomUUID().toString().replace("-", "");
-        ThreadContext.setRequestTelemetryContext(mockRequestTelemetryContext(opId));
         Short buid = (short)7;
         LocalDateTime opTimestamp = LocalDateTime.now();
         Map<String, Object> data = new TreeMap<>(
@@ -155,30 +152,22 @@ class EventLoggingServiceTest {
         String dataText = data.entrySet().stream().map(e -> e.getKey()
             + "=" + e.getValue()).collect(Collectors.joining(","));
 
-        // Act
-        service.logEvent("TestEvent", "Success", buid, "TestType",opTimestamp, data);
+        try (MockedStatic<LogUtil> logUtilMock = Mockito.mockStatic(LogUtil.class)) {
+            logUtilMock.when(LogUtil::getOrCreateOpalOperationId).thenReturn(opId);
 
-        // Assert
-        ArgumentCaptor<ILoggingEvent> captor = ArgumentCaptor.forClass(ILoggingEvent.class);
-        verify(mockAppender, atLeastOnce()).doAppend(captor.capture());
-        List<ILoggingEvent> events = captor.getAllValues();
-        String nowText = now.toString();
-        String compareTo = "TestEvent,Success," + nowText + ",TestDomain," + buid.toString()
-            + "," + opId + ",TestType," + opTimestamp + "," + dataText;
-        events.stream().findFirst().ifPresentOrElse(
-            e -> assertEquals(compareTo, e.getFormattedMessage()), () -> fail("No Log event captured."));
+            // Act
+            service.logEvent("TestEvent", "Success", buid, "TestType", opTimestamp, data);
+
+            // Assert
+            ArgumentCaptor<ILoggingEvent> captor = ArgumentCaptor.forClass(ILoggingEvent.class);
+            verify(mockAppender, atLeastOnce()).doAppend(captor.capture());
+            List<ILoggingEvent> events = captor.getAllValues();
+            String nowText = now.toString();
+            String compareTo = "TestEvent,Success," + nowText + ",TestDomain," + buid.toString()
+                + "," + opId + ",TestType," + opTimestamp + "," + dataText;
+            events.stream().findFirst().ifPresentOrElse(
+                e -> assertEquals(compareTo, e.getFormattedMessage()), () -> fail("No Log event captured."));
+        }
     }
 
-    private RequestTelemetryContext mockRequestTelemetryContext(String id) {
-        RequestTelemetryContext rtc = mock(RequestTelemetryContext.class);
-        ThreadContext.setRequestTelemetryContext(rtc);
-        RequestTelemetry rt = mock(RequestTelemetry.class);
-        when(rtc.getHttpRequestTelemetry()).thenReturn(rt);
-        TelemetryContext tc = mock(TelemetryContext.class);
-        when(rt.getContext()).thenReturn(tc);
-        OperationContext oc = mock(OperationContext.class);
-        when(tc.getOperation()).thenReturn(oc);
-        when(oc.getId()).thenReturn(id);
-        return rtc;
-    }
 }

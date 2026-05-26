a9d6cb3e2fa38bc4e1941a4256399b4783b72238
Russell Dodd <russell.dodd@hmcts.net>
2026-05-26 11:32:54 +0100

    PO-3695: Updates to allow use of 'display' name in logs.

--- src/main/java/uk/gov/hmcts/opal/common/user/authorisation/client/mapper/UserStateMapper.java
+++ src/main/java/uk/gov/hmcts/opal/common/user/authorisation/client/mapper/UserStateMapper.java
@@ -23,10 +23,12 @@ import java.util.stream.Collectors;
 public interface UserStateMapper {
 
     @Mapping(source = "username", target = "userName")
+    @Mapping(source = "name", target = "name")
     @Mapping(source = "businessUnitUsers", target = "businessUnitUser")
     UserState toUserState(UserStateDto userStateDto);
 
     @Mapping(source = "userStateV2.username", target = "userName")
+    @Mapping(source = "userStateV2.name", target = "name")
     @Mapping(target = "businessUnitUser", expression = "java(flattenBusinessUnitUsers(userStateV2, domain))")
     UserState toUserState(UserStateV2 userStateV2, Domain domain);
 

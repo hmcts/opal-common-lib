55add05a136a60718a976ee707953cdf2b83566c
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2026-06-03 15:35:03 +0100

    Updated security exception handler to also handle EntityNotFound errors (#127)

--- src/main/java/uk/gov/hmcts/opal/common/user/authentication/exception/CustomAuthenticationExceptions.java
+++ src/main/java/uk/gov/hmcts/opal/common/user/authentication/exception/CustomAuthenticationExceptions.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.opal.common.user.authentication.exception;
 
-import jakarta.servlet.ServletException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -19,14 +19,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @Slf4j(topic = "opal.CustomAuthenticationExceptions")
-@Component
+@Component("customAuthenticationExceptions2")
 public class CustomAuthenticationExceptions implements AuthenticationEntryPoint, AccessDeniedHandler {
 
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
-
+                         AuthenticationException authException) throws IOException {
+        if (checkForNotFound(response, authException)) {
+            return;
+        }
         ProblemDetail problemDetail = OpalProblemDetailFactory.createProblemDetail(
             HttpStatus.UNAUTHORIZED,
             "Unauthorized",
@@ -45,11 +47,14 @@ public class CustomAuthenticationExceptions implements AuthenticationEntryPoint,
         }
     }
 
+
     @Override
     public void handle(HttpServletRequest request,
                        HttpServletResponse response,
-                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
-
+                       AccessDeniedException accessDeniedException) throws IOException {
+        if (checkForNotFound(response, accessDeniedException)) {
+            return;
+        }
         ProblemDetail problemDetail = OpalProblemDetailFactory.createProblemDetail(
             HttpStatus.FORBIDDEN,
             "Forbidden",
@@ -66,5 +71,46 @@ public class CustomAuthenticationExceptions implements AuthenticationEntryPoint,
         try (PrintWriter writer = response.getWriter()) {
             writer.write(ToJsonString.toJsonString(problemDetail));
         }
+    }
+
+
+    private boolean checkForNotFound(HttpServletResponse response, Exception exception)
+        throws IOException {
+        log.debug("Checking if the exception is or wraps an EntityNotFoundException");
+        EntityNotFoundException entityNotFoundException = findEntityNotFoundCause(exception);
+        if (entityNotFoundException == null) {
+            return false;
+        }
+
+        log.debug("Exception chain contains EntityNotFoundException, returning 404 Not Found");
+        ProblemDetail problemDetail = OpalProblemDetailFactory.createProblemDetail(
+            HttpStatus.NOT_FOUND,
+            "Entity Not Found",
+            "The requested entity could not be found",
+            "entity-not-found",
+            false,
+            entityNotFoundException,
+            log
+        );
+        problemDetail.setProperty("reason", entityNotFoundException.getMessage());
+
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        response.setContentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE);
+
+        try (PrintWriter writer = response.getWriter()) {
+            writer.write(ToJsonString.toJsonString(problemDetail));
+        }
+        return true;
+    }
+
+    private EntityNotFoundException findEntityNotFoundCause(Throwable throwable) {
+        Throwable current = throwable;
+        while (current != null) {
+            if (current instanceof EntityNotFoundException entityNotFoundException) {
+                return entityNotFoundException;
+            }
+            current = current.getCause();
+        }
+        return null;
     }
 }

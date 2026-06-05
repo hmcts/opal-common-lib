4321ad5100a19c3b4d78348e85fcb11691fcfdcb
benedwards <ben.edwards@cgi.com>
2026-06-05 09:47:42 +0100

    Applied Patch from Jordan

--- src/test/java/uk/gov/hmcts/opal/common/dto/ToJsonStringTest.java
+++ src/test/java/uk/gov/hmcts/opal/common/dto/ToJsonStringTest.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.opal.common.dto;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ToJsonStringTest {
 
@@ -21,5 +23,59 @@ public class ToJsonStringTest {
                                                        "number_of_failed_login_attempts":25
                                                        }
                                                        """);
+    }
+
+    @Test
+    void objectToPrettyJson_shouldProduceSnakeCase() {
+        var testDto = new TestDto("Harry", "Smith", 25);
+
+        String json = ToJsonString.objectToPrettyJson(testDto);
+
+        assertAll(
+            () -> assertTrue(json.contains("first_name")),
+            () -> assertTrue(json.contains("surname")),
+            () -> assertTrue(json.contains("number_of_failed_login_attempts"))
+        );
+    }
+
+    @Test
+    void objectToPrettyJsonsString_shouldProduceSnakeCase() {
+        var testDto = new TestDto("Harry", "Smith", 25);
+
+        String json = ToJsonString.objectToPrettyJson(testDto);
+
+        assertAll(
+            () -> json.contains("first_name"),
+            () -> json.contains("surname"),
+            () -> json.contains("number_of_failed_login_attempts")
+        );
+    }
+
+    @Test
+    void toPrettyJsonsString_shouldNotAlterPropertyCase() {
+        String sourceJson = """
+            {"propOne":"some value","propTwo":"another value"}
+            """;
+
+        String json = ToJsonString.toPrettyJson(sourceJson);
+
+        assertAll(
+            () -> json.contains("propOne"),
+            () -> json.contains("propTwo")
+        );
+    }
+
+    @Test
+    void objectToJson_doesNotThrowOnNullInput() {
+        String json = ToJsonString.objectToJson(null);
+
+        assertThat(json).isEqualTo("null");
+    }
+
+    @Test
+    void objectToPrettyJson_doesNotThrowOnNullInput() {
+        String json = ToJsonString.objectToPrettyJson(null);
+
+        assertThat(json).isEqualTo("null");
     }
 }
